fff2570631b21b0e201ed4e9a35093a72d0a0f1d
Felix Heidecke <felix@heidecke.me>
2018-12-03 12:56:46 +0100

    Initial frontend commit (setup)

---
+++ gruntfile.js
@@ -0,0 +1,72 @@
+const sass = require('node-sass');
+
+module.exports = function (grunt) {
+	grunt.initConfig({
+		sass: {
+			options: {
+				implementation: sass,
+				sourcemap: false
+			},
+			dist: {
+				files: {
+					'css/files_mediaviewer.css': 'src/styles/default.scss'
+				}
+			}
+		},
+
+		browserify: {
+			dist: {
+				files: {
+					'js/files_mediaviewer.js': 'src/scripts/default.js'
+				},
+				options: {
+					transform: [
+						['babelify', {
+							presets: 'es2015'
+						}],
+						['vueify']
+
+					],
+					browserifyOptions: {
+						debug: true
+					}
+				}
+			}
+		},
+
+		watch: {
+			default: {
+				options: {
+					spawn: false
+				},
+				files: [
+					'src/**/*.js',
+					'src/**/*.scss',
+					'src/**/*.vue'
+				],
+				tasks: [
+					'force:on',
+					'sass',
+					'browserify',
+					'force:off'
+				]
+			}
+		}
+	}); //initConfig
+	//
+	grunt.loadNpmTasks('grunt-force');
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-browserify');
+
+	grunt.registerTask('default', [
+		'sass',
+		'browserify'
+	]);
+
+	grunt.registerTask('watcher', [
+		'sass',
+		'browserify',
+		'watch'
+	]);
+};
