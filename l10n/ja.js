cf8ffc727cd80b32443be71cb366a44f22fd753d
Thomas Müller <thomas.mueller@tmit.eu>
2020-05-26 03:57:33 +0000

    [tx] updated from transifex

---
+++ l10n/ja.js
@@ -0,0 +1,18 @@
+OC.L10N.register(
+    "files_mediaviewer",
+    {
+    "Close" : "閉じる",
+    "Download" : "ダウンロード",
+    "Fullscreen" : "フルスクリーン",
+    "Loading" : "読込中",
+    "Mute" : "ミュート",
+    "Next" : "次",
+    "of" : "of",
+    "Play" : "再生",
+    "Previous" : "前",
+    "Replay" : "リプレイ",
+    "Rotate 90° counterclockwise" : "90°左回り",
+    "Zoom in" : "ズームイン",
+    "Zoom out" : "ズームアウト"
+},
+"nplurals=1; plural=0;");
