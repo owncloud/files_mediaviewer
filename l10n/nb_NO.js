1298fa4ddbae7ed0004f25c3919d23e86b95a312
Thomas Müller <thomas.mueller@tmit.eu>
2020-07-12 00:07:41 +0000

    [tx] updated from transifex

---
+++ l10n/nb_NO.js
@@ -0,0 +1,18 @@
+OC.L10N.register(
+    "files_mediaviewer",
+    {
+    "Close" : "Lukk",
+    "Download" : "Last ned",
+    "Fullscreen" : "Fullskjerm",
+    "Loading" : "Laster",
+    "Mute" : "Demp",
+    "Next" : "Neste",
+    "of" : "av",
+    "Play" : "Spill",
+    "Previous" : "Forrige",
+    "Replay" : "Spill på nytt",
+    "Rotate 90° counterclockwise" : "Roter 90° mot klokka",
+    "Zoom in" : "Zoom inn",
+    "Zoom out" : "Zoom ut"
+},
+"nplurals=2; plural=(n != 1);");
