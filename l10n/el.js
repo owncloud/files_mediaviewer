572b2fe3221d66813032d9bbfbe9ad582d18eb71
Thomas Müller <thomas.mueller@tmit.eu>
2020-08-06 01:36:17 +0000

    [tx] updated from transifex

---
+++ l10n/el.js
@@ -0,0 +1,15 @@
+OC.L10N.register(
+    "files_mediaviewer",
+    {
+    "Close" : "Κλείσιμο",
+    "Download" : "Λήψη",
+    "Fullscreen" : "Πλήρης οθόνη",
+    "Loading" : "Γίνεται φόρτωση",
+    "Next" : "Επόμενο",
+    "Play" : "Αναπαραγωγή",
+    "Previous" : "Προηγούμενο",
+    "Replay" : "Επανάληψη",
+    "Zoom in" : "Μεγέθυνση",
+    "Zoom out" : "Σμίκρυνση"
+},
+"nplurals=2; plural=(n != 1);");
