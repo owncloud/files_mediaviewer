b67fec306cafdedfb04b79fdb5bf8bf0dcd876af
Thomas Müller <thomas.mueller@tmit.eu>
2020-09-21 01:22:55 +0000

    [tx] updated from transifex

---
+++ l10n/zh_TW.js
@@ -0,0 +1,17 @@
+OC.L10N.register(
+    "files_mediaviewer",
+    {
+    "Close" : "關閉",
+    "Download" : "下載",
+    "Fullscreen" : "全螢幕",
+    "Loading" : "載入中",
+    "Mute" : "靜音",
+    "Next" : "下一個",
+    "Play" : "播放",
+    "Previous" : "上一個",
+    "Replay" : "重播",
+    "Rotate 90° counterclockwise" : "逆時針旋轉 90°",
+    "Zoom in" : "放大",
+    "Zoom out" : "縮小"
+},
+"nplurals=1; plural=0;");
