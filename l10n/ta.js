18f238aea4bf6b264088aaca01662587ef3c7c95
Benedikt Kulmann <benedikt@kulmann.biz>
2021-02-28 00:11:36 +0000

    [tx] updated from transifex

---
+++ l10n/ta.js
@@ -0,0 +1,11 @@
+OC.L10N.register(
+    "files_mediaviewer",
+    {
+    "Close" : "மூடுக",
+    "Download" : "பதிவிறக்குக",
+    "Next" : "அடுத்த",
+    "of" : "உடைய",
+    "Play" : "Play",
+    "Previous" : "முன்தைய"
+},
+"nplurals=2; plural=(n != 1);");
