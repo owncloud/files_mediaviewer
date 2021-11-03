c6da829ce7f9d7a1fb669a02f2fb20dac8dec8bc
ownClouders <devops@owncloud.com>
2021-11-03 00:05:52 +0000

    [tx] updated from transifex

--- l10n/ar.js
+++ l10n/ar.js
@@ -13,6 +13,7 @@ OC.L10N.register(
     "Replay" : "إعادة التشغيل",
     "Rotate 90° counterclockwise" : "تدوير 90° درجة عكس اتجاه عقارب الساعة",
     "Zoom in" : "تكبير",
-    "Zoom out" : "تصغير"
+    "Zoom out" : "تصغير",
+    "Open in Media Viewer" : "فتح في عارض الوسائط"
 },
 "nplurals=6; plural=n==0 ? 0 : n==1 ? 1 : n==2 ? 2 : n%100>=3 && n%100<=10 ? 3 : n%100>=11 && n%100<=99 ? 4 : 5;");
