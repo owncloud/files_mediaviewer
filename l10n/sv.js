89b928bab92f96dd5cd0f10eb1bfc43ec63b1f3e
ownClouders <devops@owncloud.com>
2022-03-22 00:56:55 +0000

    [tx] updated from transifex

---
+++ l10n/sv.js
@@ -0,0 +1,19 @@
+OC.L10N.register(
+    "files_mediaviewer",
+    {
+    "Close" : "Stäng",
+    "Download" : "Ladda ned",
+    "Fullscreen" : "Fullskärm",
+    "Loading" : "Laddar",
+    "Mute" : "Ljudlös",
+    "Next" : "Nästa",
+    "of" : "av",
+    "Play" : "Spela",
+    "Previous" : "Föregående",
+    "Replay" : "Repris",
+    "Rotate 90° counterclockwise" : "Rotera 90° moturs",
+    "Zoom in" : "Zooma in",
+    "Zoom out" : "Zooma ut",
+    "Open in Media Viewer" : "Öppna i Mediavisaren"
+},
+"nplurals=2; plural=(n != 1);");
