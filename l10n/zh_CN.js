95f720b0d9f67117a42383a6ec2076cc808d6a41
ownClouders <devops@owncloud.com>
2022-04-26 01:23:10 +0000

    [tx] updated from transifex

---
+++ l10n/zh_CN.js
@@ -0,0 +1,19 @@
+OC.L10N.register(
+    "files_mediaviewer",
+    {
+    "Close" : "关闭",
+    "Download" : "下载",
+    "Fullscreen" : "全屏",
+    "Loading" : "加载中",
+    "Mute" : "静音",
+    "Next" : "下一个",
+    "of" : "在",
+    "Play" : "播放",
+    "Previous" : "上一个",
+    "Replay" : "重播",
+    "Rotate 90° counterclockwise" : "逆时针旋转 90°",
+    "Zoom in" : "放大",
+    "Zoom out" : "缩小",
+    "Open in Media Viewer" : "使用媒体查看器打开"
+},
+"nplurals=1; plural=0;");
