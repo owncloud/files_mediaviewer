a4b5d1dda77e9922506cfebe6bd69bff9836c845
ownClouders <devops@owncloud.com>
2022-05-24 00:36:24 +0000

    [tx] updated from transifex

--- l10n/es.js
+++ l10n/es.js
@@ -16,4 +16,4 @@ OC.L10N.register(
     "Zoom out" : "Alejar",
     "Open in Media Viewer" : "Abrir con Media Viewer"
 },
-"nplurals=2; plural=(n != 1);");
+"nplurals=3; plural=n == 1 ? 0 : n != 0 && n % 1000000 == 0 ? 1 : 2;");
