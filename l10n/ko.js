ea26755b71eab6bdb6c9e3dbce9f68015b60bd92
ownClouders <devops@owncloud.com>
2022-06-26 00:10:48 +0000

    [tx] updated from transifex

---
+++ l10n/ko.js
@@ -0,0 +1,18 @@
+OC.L10N.register(
+    "files_mediaviewer",
+    {
+    "Close" : "닫기",
+    "Download" : "다운로드",
+    "Fullscreen" : "전체화면",
+    "Loading" : "로딩",
+    "Mute" : "음소거",
+    "Next" : "다음",
+    "Play" : "재생",
+    "Previous" : "이전",
+    "Replay" : "다시 재생",
+    "Rotate 90° counterclockwise" : "시계방향으로 90도 회전",
+    "Zoom in" : "확대",
+    "Zoom out" : "축소",
+    "Open in Media Viewer" : "미디어 뷰어로 열기"
+},
+"nplurals=1; plural=0;");
