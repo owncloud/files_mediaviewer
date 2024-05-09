f2814dc6c17c1fb83c34d8d855688afff07ff2cd
ownClouders <devops@owncloud.com>
2024-05-09 00:07:48 +0000

    [tx] updated from transifex

---
+++ l10n/es_ES.js
@@ -0,0 +1,9 @@
+OC.L10N.register(
+    "files_mediaviewer",
+    {
+    "Close" : "Cerrar",
+    "Next" : "Siguiente",
+    "Play" : "Reproducir",
+    "Previous" : "Previo"
+},
+"nplurals=3; plural=n == 1 ? 0 : n != 0 && n % 1000000 == 0 ? 1 : 2;");
