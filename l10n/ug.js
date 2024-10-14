734759a2de141f10580a94abd4816fd25698fb49
ownClouders <devops@owncloud.com>
2024-10-14 01:38:59 +0000

    [tx] updated from transifex

---
+++ l10n/ug.js
@@ -0,0 +1,19 @@
+OC.L10N.register(
+    "files_mediaviewer",
+    {
+    "Close" : "تاقاش",
+    "Download" : "چۈشۈرۈش",
+    "Fullscreen" : "تولۇق ئېكران",
+    "Loading" : "يۈكلەۋاتىدۇ",
+    "Mute" : "Mute",
+    "Next" : "كېيىنكى",
+    "of" : "of",
+    "Play" : "قوي",
+    "Previous" : "ئالدىنقى",
+    "Replay" : "Replay",
+    "Rotate 90° counterclockwise" : "سائەتكە قارشى 90 ° ئايلاندۇرۇڭ",
+    "Zoom in" : "چوڭايتىش",
+    "Zoom out" : "چوڭايتىڭ",
+    "Open in Media Viewer" : "Media Viewer دا ئېچىڭ"
+},
+"nplurals=2; plural=(n != 1);");
