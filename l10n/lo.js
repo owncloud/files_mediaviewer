cd010f58a3eedc14518c30904266dae2d5954953
ownClouders <devops@owncloud.com>
2025-11-16 00:32:11 +0000

    [tx] updated from transifex

---
+++ l10n/lo.js
@@ -0,0 +1,10 @@
+OC.L10N.register(
+    "files_mediaviewer",
+    {
+    "Close" : "ປິດ",
+    "Download" : "ດາວໂຫຼດ",
+    "Loading" : "ກຳລັງໂຫຼດ...",
+    "Next" : "ຕໍ່ໄປ",
+    "of" : "ຈາກ"
+},
+"nplurals=1; plural=0;");
