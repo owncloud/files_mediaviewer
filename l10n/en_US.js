accddbdbd72498b9ccd1a3d75240d63380b3e65a
ownClouders <devops@owncloud.com>
2025-12-02 00:06:23 +0000

    [tx] updated from transifex

---
+++ l10n/en_US.js
@@ -0,0 +1,14 @@
+OC.L10N.register(
+    "files_mediaviewer",
+    {
+    "Close" : "Close",
+    "Download" : "Download",
+    "Fullscreen" : "Fullscreen",
+    "Loading" : "Loading",
+    "Replay" : "Replay",
+    "Rotate 90° counterclockwise" : "Rotate 90° counterclockwise",
+    "Zoom in" : "Zoom in",
+    "Zoom out" : "Zoom out",
+    "Open in Media Viewer" : "Open in Media Viewer"
+},
+"nplurals=2; plural=(n != 1);");
